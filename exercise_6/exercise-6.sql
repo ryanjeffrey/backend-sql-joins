efd8b993594f5d1b8353d51f9bdd21fe30d3cac0
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-11-09 17:32:01 -0600

    Solve exercise 6

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,7 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+SELECT title FROM customer
+LEFT JOIN rental on customer.customer_id = rental.customer_id
+LEFT JOIN inventory on rental.inventory_id = inventory.inventory_id
+LEFT JOIN film on inventory.film_id = film.film_id
+WHERE first_name = 'Roberta'
