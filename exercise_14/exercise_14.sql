97ebf8b90dfd2ffec430e9f2b27058ec04466882
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-11-09 19:14:52 -0600

    Solve exercise 14

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,7 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT category.name, AVG(film.length)
+FROM category
+LEFT JOIN film_category on film_category.category_id = category.category_id
+LEFT JOIN film on film_category.film_id = film.film_id
+GROUP by category.name
